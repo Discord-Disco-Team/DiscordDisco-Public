fa6f560a981cf90f8cf5cd86df56bf12608f9c84
blakrr <blakeyman5@gmail.com>
2024-09-16 18:22:16 -0400

    one more push i fucked up

--- example_mods/Discord Disco/scripts/voices.hx
+++ example_mods/Discord Disco/scripts/voices.hx
@@ -3,9 +3,11 @@ import backend.Mods;
 
 using StringTools;
 
-function onCreatePost()
+function onSongStart()
 {
+    
     if (FileSystem.exists("mods/" + Mods.currentModDirectory + "/songs/" + StringTools.replace(game.curSong.toLowerCase(), " ", "-") + "/Voices-" + game.boyfriend.curCharacter + ".ogg")) {
+        debugPrint('test');
         Paths.voices(game.curSong, game.boyfriend.curCharacter);
         game.vocals.loadEmbedded(Paths.returnSound(null, StringTools.replace(game.curSong.toLowerCase(), " ", "-") + "/Voices-" + game.boyfriend.curCharacter, 'songs'));
     }
