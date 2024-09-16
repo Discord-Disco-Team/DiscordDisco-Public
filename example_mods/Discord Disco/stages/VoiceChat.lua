3f071a918704e4d9955cce2ad6872925492322e2
DevilishFox82 <joshuawyman@icloud.com>
2024-09-16 16:43:04 -0400

    the overlay shit

--- example_mods/Discord Disco/stages/VoiceChat.lua
+++ example_mods/Discord Disco/stages/VoiceChat.lua
@@ -1,12 +1,36 @@
 function onCreate()
 	-- background shit
+	makeLuaSprite('stageback', 'black', 0, 0);
+	setScrollFactor('stageback', 0.84, 0.87);
+	scaleObject('stageback', 10, 10)
 	
-	makeLuaSprite('voiceoverlay', 'voiceoverlay', -500, -1250);
-	setLuaSpriteScrollFactor('voiceoverlay', 1.0, 1.0);
-	scaleObject('voiceoverlay', 5, 5);
+	makeLuaSprite('stageback2', 'voiceoverlay', -60, -40);
+	setScrollFactor('stageback2', 0, 0);
+	scaleObject('stageback2', 0.87, 0.8);
 
+    addLuaSprite('stageback', false)
+    addLuaSprite('stagefront', false);
+    addLuaSprite('stageback3', true)
+    addLuaSprite('stageback2', true)
+	
+	end
 
-	
-	addLuaSprite('voiceoverlay', true);
-	
-end
+    function onEvent(name,value1,value2)
+    	if name == 'Play Animation' then
+    		if value2 == 0 then
+
+    	setProperty('stageback.visible', true)
+    	setProperty('stagefront.visible', true)
+    	setProperty('stageback2.visible', true)	
+    	setProperty('stageback3.visible', false)
+    end
+    
+    	    if value2 == 1 then
+
+        setProperty('stageback.visible', true)
+    	setProperty('stagefront.visible', true)
+    	setProperty('stageback2.visible', false)	
+    	setProperty('stageback3.visible', true)
+    end
+    end
+    end	    		
