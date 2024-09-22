1c22306feebdd1fbea8c6395c3f2333f74c0ac9b
Nina <85642496+Northernite@users.noreply.github.com>
2024-09-22 15:18:32 -0400

    fixed shit

---
+++ example_mods/Discord Disco/data/Lore/strumlines.lua
@@ -0,0 +1,5 @@
+function onCreatePost()
+    for strumLineNotes = 0,3 do
+        setPropertyFromGroup('strumLineNotes',strumLineNotes,'visible',false)
+    end
+end
--- example_mods/Discord Disco/scripts/ruined.lua
+++ example_mods/Discord Disco/scripts/ruined.lua
@@ -9,9 +9,7 @@ function onUpdate()
    elseif keyJustPressed('debug_1') and keyPressed('up') then
       setProperty('allowDebugKeys', true);
    end
-end
 
-function onUpdate()
    if keyJustPressed('debug_2') and not keyPressed('up') then
       loadSong('Ruined')
       setPropertyFromClass('PlayState', 'chartingMode', false)
