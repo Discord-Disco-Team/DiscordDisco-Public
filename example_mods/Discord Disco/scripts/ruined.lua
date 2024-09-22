b85bc69293c80caeb9a3e643d0da96494dc16773
Nina <85642496+Northernite@users.noreply.github.com>
2024-09-22 18:52:31 -0400

    ruined

--- example_mods/Discord Disco/scripts/ruined.lua
+++ example_mods/Discord Disco/scripts/ruined.lua
@@ -3,17 +3,10 @@ function onCreatePost()
 end
 
 function onUpdate()
-   if keyJustPressed('debug_1') and not keyPressed('up') then
+   if keyJustPressed('debug_1') and keyPressed('up') or keyJustPressed('debug_2') and keyPressed('up') then
+      setProperty('allowDebugKeys', true)
+   elseif keyJustPressed('debug_1') and not keyPressed('up') or keyJustPressed('debug_2') and not keyPressed('up') then
       loadSong('Ruined')
       setPropertyFromClass('PlayState', 'chartingMode', false)
-   elseif keyJustPressed('debug_1') and keyPressed('up') then
-      setProperty('allowDebugKeys', true);
-   end
-
-   if keyJustPressed('debug_2') and not keyPressed('up') then
-      loadSong('Ruined')
-      setPropertyFromClass('PlayState', 'chartingMode', false)
-   elseif keyJustPressed('debug_2') and keyPressed('up') then
-      setProperty('allowDebugKeys', true);
    end
 end
