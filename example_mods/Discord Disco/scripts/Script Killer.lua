f0b73f3310f4791ac593b4e94c5e75340151fcb9
Nina <85642496+Northernite@users.noreply.github.com>
2024-11-23 14:41:46 -0500

    Gameovers

--- example_mods/Discord Disco/scripts/Script Killer.lua
+++ example_mods/Discord Disco/scripts/Script Killer.lua
@@ -2,4 +2,10 @@ function onCreate()
   if songName == "Gaylord" then
     removeLuaScript('scripts/ghostNoteTrail')
   end
+  if songName == "Stainless" then
+    removeLuaScript('scripts/ghostNoteTrail')
+  end
+  if songName == "Stainless-nitro" then
+    removeLuaScript('scripts/ghostNoteTrail')
+  end
 end
