1c85ea548b3391ca71262d815130a2d183f580a4
DevilishFox82 <joshuawyman@icloud.com>
2024-12-24 00:00:21 -0500

    the lyrics

---
+++ example_mods/Discord Disco/custom_events/goidlyrics.lua
@@ -0,0 +1,27 @@
+function onEvent(name, value1, value2)
+    local var string = (value1)
+    local var length = tonumber(0 + value2)
+    if name == "goidlyrics" then
+
+        makeLuaText('captions', 'Lyrics go here', 1000, 150, 500)
+        setTextString('captions',  '' .. string)
+        setTextFont('captions', 'vcr.ttf')
+        setTextColor('captions', '0xA1A1A1')
+        setTextSize('captions', 30);
+        addLuaText('captions')
+        setObjectCamera('captions', 'other');
+        setTextAlignment('captions', 'center')
+        runTimer('lyricalTho', length, 1)
+        --removeLuaText('captions', true)
+	   makeLuaSprite('image', 'Goid', 600, 375, 500);
+	   addLuaSprite('image', true);
+	   setObjectCamera('image', 'other');
+    end
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+    if tag == 'lyricalTho' then
+        removeLuaText('captions', true)
+        removeLuaSprite('image', true);
+    end
+end
